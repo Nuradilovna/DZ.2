cda42f31aa2f039bda5a94d257e2e59684f24a8c
Nuradilovna <2303.02011@manas.edu.kg>
2024-12-28 14:04:28 +0600

    dz.8

--- js/lesson.js
+++ js/lesson.js
@@ -1,68 +1,45 @@
-//PHONE BLOCK
 
-// const phoneInput=document.querySelector('#phone_input');
-// const phoneButton=document.querySelector('#phone_button');
-// const phoneResult=document.querySelector('#phone_result');
-//
-// const regExp=/^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
-//
-// phoneButton.onclick=()=>{
-//     if(regExp.test(phoneInput.value)){
-//        phoneResult.innerHTML="ok";
-//        phoneResult.style.color="green"
-//     }else {
-//         phoneResult.innerHTML="Invalid phone number";
-//         phoneResult.style.color="red"
-//     }
-// }
+//CARD SWITCHER
+const cardBlock = document.querySelector(".card_s");
+const btnNext = document.querySelector("#btn-next");
+const btnPrev = document.querySelector("#btn-prev");
 
-// // Tab slider
-// const tabContentBlocks = document.querySelectorAll(".tab_content_block");
-// const tabs=document.querySelectorAll(".tab_content_item");
-// const tabsParent=document.querySelector(".tab_content_items")
-// let index = 0
-// let interval
-//
-// const hideTabContent=()=>{
-//     tabContentBlocks.forEach(item=>{
-//         item.style.display="none";
-//     })
-//     tabs.forEach(item=>{
-//         item.classList.remove("tab_content_item_active");
-//     })
-// }
-// const showTabContent=(index= 0)=>{
-//     tabContentBlocks[index].style.display="block";
-//     tabs[index].classList.add("tab_content_item_active");
-// }
-// hideTabContent();
-// showTabContent();
-//
-// const autoTabs = () => {
-//     interval = setInterval(() => {
-//         hideTabContent();
-//         index ++ >= tabs.length ? index = 0 : index = index ++;
-//         showTabContent(index);
-//     }, 5000);
-// };
-//
-// autoTabs();
-//
-//
-// tabsParent.onclick=(e)=>{
-//     if (e.target.classList.contains('tab_content_item')) {
-//         tabs.forEach((item,newIndex)=>{
-//             if (e.target===item){
-//                 clearInterval(interval);
-//                 index = newIndex;
-//                 hideTabContent();
-//                 showTabContent(index);
-//                 autoTabs();
-//             }
-//         })
-//
-//     }
-// }
+let cardId = 1;
+
+const CardSwitcher = async (id) => {
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+        const data = await response.json();
+        const { title, completed, id: cardId } = data;
+
+        cardBlock.innerHTML = `
+            <p>${title}</p>
+            <p>${completed}</p>
+            <span>${cardId}</span>
+        `;
+    } catch (error) {
+        console.error("Error loading card:", error);
+        cardBlock.innerHTML = "Error loading card";
+    }
+};
+CardSwitcher(cardId);
+
+btnNext.onclick = () => {
+    cardId++;
+    if (cardId > 200) {
+        cardId = 1;
+    }
+    CardSwitcher(cardId);
+};
+
+btnPrev.onclick = () => {
+    cardId--;
+    if (cardId < 1) {
+        cardId = 200;
+    }
+    CardSwitcher(cardId);
+};
+
 
 //OPEN WATER
 
